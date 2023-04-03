e131375b3bdf3c0902e86d56c3d80d3a758911d0
wobbethan <wobbethan@gmail.com>
2023-04-03 10:20:20 -0400

    Generated SQL start

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import GraphMenu from './components/graphMenu.js'
 import Login from './components/login'
 import Graphdisplay from './components/graph'
 import Profile from './components/profile'
+import { useState } from 'react';
 
 //import background from './assets/planebg.png'
 
 function App() {
+  const [filters, setFilters] = useState({})
+
+  const updateFilters = (searchFilters) => {
+    setFilters(searchFilters);
+  }
+
+
   return (
     <div style={{
       display: "flex",
@@ -19,7 +27,10 @@ function App() {
       <div>
       <Login/>
       <Profile/>
-      <GraphMenu/>
+      <GraphMenu callback={updateFilters}/>
+      <h3>SELECT *</h3>
+      <h3>FROM (tables)</h3>
+      <h3>WHERE DelayType = "" AND FlightNumber = "{"flightNum" in filters ? filters["flightNum"] : "FLIGHTNUM" }" AND airlineCode = "{"airline" in filters ? filters["airline"] : "AIRLINE" }" AND airportStart = "{"airportStart" in filters ? filters["airportStart"] : "AIRPORT START" }" AND airportEnd = "{"airportEnd" in filters ? filters["airportEnd"] : "AIRPORT END" }" </h3>
       <Graphdisplay/>
       </div>
 
--- src/components/graphMenu.js
+++ src/components/graphMenu.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactSlider from 'react-slider'
 
-function graphMenu() {
+function GraphMenu(props) {
+
+  //Constants
+    const [airline, setAirline] = useState("")
+    const [airportStart, setAirportStart] = useState("")
+    const [airportEnd, setAirportEnd] = useState("")
+    const [flightNum, setFlightNum] = useState(0)
+
+    const generateButtonPressed = () => {
+      props.callback({airline: airline, airportStart: airportStart, airportEnd: airportEnd, flightNum: flightNum})
+
+    }
+
+
+
   return (
     <div>
       <h1 className="d-flex justify-content-center">Menu</h1>
+      
       <h2>Delay Type (Y-axis | Up to 5 curves )</h2>
       <div>
       <label>
@@ -68,23 +83,25 @@ function graphMenu() {
         </label>
         <form>
               <label htmlFor="airline-field">Airline code:</label>
-              <input id="airine-field" value = "" type="text" ></input>
+              <input id="airline-field"  type="text" value ={airline} onChange={(e) => setAirline(e.target.value)}></input>
+
               <label htmlFor="airportStart-field">Starting Airport Code:</label>
-              <input id="airportStart-field" value = "" type="text" ></input>
+              <input id="airportStart-field" type="text" value ={airportStart} onChange={(e) => setAirportStart(e.target.value)}></input>
+
               <label htmlFor="airportEnd-field">Destination Airport code:</label>
-              <input id="airportEnd-field" value = "" type="text" ></input>
+              <input id="airportEnd-field"  type="text" value ={airportEnd} onChange={(e) => setAirportEnd(e.target.value)}></input>
+
               <label htmlFor="flightnum-field">Flight Number:</label>
-              <input id="airport-field" value = "" type="text" ></input>
+              <input id="airport-field"  type="number" value ={flightNum} onChange={(e) => setFlightNum(e.target.value)}></input>
         </form>
 
-        <button type="button">Generate</button>
+        <button type="button" onClick={generateButtonPressed}>Generate</button>
 
       </div>
-      <h3> THIS LABEL WILL DISPLAY GENERATED SQL FOR TESTING </h3>
 
     </div>
 
   )
 }
 
-export default graphMenu
+export default GraphMenu
