c58dddd0578b74bbadf9a9458e5bfb938bf109b9
wobbethan <wobbethan@gmail.com>
2023-04-03 14:35:21 -0400

    Check boxes not working

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ function App() {
       <GraphMenu callback={updateFilters}/>
       <h3>SELECT *</h3>
       <h3>FROM (tables)</h3>
-      <h3>WHERE DelayType = "" AND FlightNumber = "{"flightNum" in filters ? filters["flightNum"] : "FLIGHTNUM" }" AND airlineCode = "{"airline" in filters ? filters["airline"] : "AIRLINE" }" AND airportStart = "{"airportStart" in filters ? filters["airportStart"] : "AIRPORT START" }" AND airportEnd = "{"airportEnd" in filters ? filters["airportEnd"] : "AIRPORT END" }" </h3>
+      <h3>WHERE DelayType = "ANY" AND FlightNumber = "{"flightNum" in filters ? filters["flightNum"] : "ANY" }" AND airlineCode = "{"airline" in filters ? filters["airline"] : "ANY" }" AND airportStart = "{"airportStart" in filters ? filters["airportStart"] : "ANY" }" AND airportEnd = "{"airportEnd" in filters ? filters["airportEnd"] : "ANY" }"  </h3>
+      <h3>AND Cancelled = "{"cancelled" in filters ? filters["cancelled"] : "false" }" AND diverted = "{"diverted" in filters ? filters["diverted"] : "false" }" AND International = "{"international" in filters ? filters["international"] : "false" }" AND domestic = "{"domestic" in filters ? filters["domestic"] : "false" }"  </h3>
+
       <Graphdisplay/>
       </div>
 
